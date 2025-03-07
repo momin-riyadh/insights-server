5cb51a8ee5b1ca083104875b6378c93967137a3b
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2025-03-07 12:01:04 +0600

    fix: migration added

--- prisma/migrations/20250307054010_init/migration.sql
+++ prisma/migrations/20250307054010_init/migration.sql
@@ -2,21 +2,12 @@
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `role` VARCHAR(191) NOT NULL,
-    `companyId` INTEGER NOT NULL,
+    `company` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `Company` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `logo` VARCHAR(191) NULL,
-
-    UNIQUE INDEX `Company_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -37,9 +28,6 @@ CREATE TABLE `_UserPermissions` (
     UNIQUE INDEX `_UserPermissions_AB_unique`(`A`, `B`),
     INDEX `_UserPermissions_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `User` ADD CONSTRAINT `User_companyId_fkey` FOREIGN KEY (`companyId`) REFERENCES `Company`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `_UserPermissions` ADD CONSTRAINT `_UserPermissions_A_fkey` FOREIGN KEY (`A`) REFERENCES `Permission`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
